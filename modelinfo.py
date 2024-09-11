a9f6b660734ec8df8dcc30d619da15888cc9cbc8
PureBacn <59106892+PureBacn@users.noreply.github.com>
2024-09-11 11:47:41 -0700

    Add files via upload

---
+++ modelinfo.py
@@ -0,0 +1,37 @@
+import onnx
+
+
+def get(path):
+    try:
+        model = onnx.load(path)
+    except:
+        print("Model Failed To lOad")
+
+    output = [node.name for node in model.graph.output]
+
+    input_all = [node.name for node in model.graph.input]
+    input_initializer =  [node.name for node in model.graph.initializer]
+    net_feed_input = list(set(input_all)  - set(input_initializer))
+
+    print("Inputs: ", net_feed_input)
+    print("Input Parameters:")
+    for input in model.graph.input:
+        print (input.name, end=": ")
+        # get type of input tensor
+        tensor_type = input.type.tensor_type
+        # check if it has a shape:
+        if (tensor_type.HasField("shape")):
+            # iterate through dimensions of the shape:
+            for d in tensor_type.shape.dim:
+                # the dimension may have a definite (integer) value or a symbolic identifier or neither:
+                if (d.HasField("dim_value")):
+                    print (d.dim_value, end=", ")  # known dimension
+                elif (d.HasField("dim_param")):
+                    print (d.dim_param, end=", ")  # unknown dimension with symbolic name
+                else:
+                    print ("?", end=", ")  # unknown dimension with no name
+        else:
+            print ("unknown rank", end="")
+        print()
+
+    print("Outputs: ", output)
