dbbf5b9aca22dd3a72c8a38e9f968f3b790f66f3
PureBacn <nathanhii007@outlook.com>
2024-09-20 18:19:43 -0700

    Update ResistiveTouchScreen.ino

--- ResistiveTouchScreen/ResistiveTouchScreen.ino
+++ ResistiveTouchScreen/ResistiveTouchScreen.ino
@@ -1,77 +1,29 @@
-/*=================================
-  This code demostrates 4-Wire Touch screen
-  interfacing with Arduino
+#include "TouchScreen.h"
+#include <AccelStepper.h>
 
-  blog.circuits4you.com
-  www.circuits4you.com
-
-  4- Wire Touchscreen Connections
-  A0=====X+
-  A1=====X-
-  A2=====Y+
-  A3=====Y-
-  =================================*/
-
-#include <Stepper.h>
-
-  
 //Define your Touch screen connections
 #define X1 A0
-#define X2 A1
-#define Y1 A2
-#define Y2 A3
-//Define your screen resolution as per your Touch screen (Max: 1024)
-#define Xresolution 320 //128
-#define Yresolution 240 //64
+#define X2 A2
+#define Y1 A3
+#define Y2 A1
 
-#define FULLSTEP 8
-
-
-float angles[3];
+TouchScreen ts = TouchScreen(X1,Y1,X2,Y2,0);
+AccelStepper stepper(AccelStepper::DRIVER, 2, 5);
 
 void setup()
 {
-  Serial.begin(9600);
+   Serial.begin(9600);
+   pinMode(4,OUTPUT);
+    stepper.setMaxSpeed(200);      // Set desired speed
+    stepper.setAcceleration(100);   // Set acceleration
+    stepper.moveTo(10000);
 }
 
 void loop()
 {
-  int X, Y; //Touch Coordinates are stored in X,Y variable
-  pinMode(Y1, INPUT);
-  pinMode(Y2, INPUT);
-  digitalWrite(Y2, LOW);
-  pinMode(X1, OUTPUT);
-  digitalWrite(X1, HIGH);
-  pinMode(X2, OUTPUT);
-  digitalWrite(X2, LOW);
-  X = (analogRead(Y1)) / (1024 / Xresolution); //Reads X axis touch position
-
-  pinMode(X1, INPUT);
-  pinMode(X2, INPUT);
-  digitalWrite(X2, LOW);
-  pinMode(Y1, OUTPUT);
-  digitalWrite(Y1, HIGH);
-  pinMode(Y2, OUTPUT);
-  digitalWrite(Y2, LOW);
-  Y = (analogRead(X1)) / (1024 / Yresolution); //Reads Y axis touch position
-
-  //Display X and Y on Serial Monitor
-  Serial.print(X);
+  TSPoint p = ts.getPoint();
+  Serial.print((p.x-100.0)/810.0);
   Serial.print(" ");
-  Serial.println(Y);
-  delay(100);
-
-  while (Serial.available() < 0) {
-    delay(100);
-  }
-  String a0 = Serial.readString();
-  a0.trim();
-  angles[0] = a0.toFloat();
-  String a1 = Serial.readString();
-  a1.trim();
-  angles[1] = a1.toFloat();
-  String a2 = Serial.readString();
-  a2.trim();
-  angles[2] = a2.toFloat();
-  Serial.println(a0 + " " + a1 + " " + a2);
+  Serial.println((p.y-90.0)/850.0);
+  stepper.runSpeed();
 }
